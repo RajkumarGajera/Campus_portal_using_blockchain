ab9af51cb9cc7540faa25d536004c6749f78afd4
RajkumarGajera <69236171+RajkumarGajera@users.noreply.github.com>
2020-10-10 09:21:58 +0530

    Add files via upload

---
+++ abi.js
@@ -0,0 +1,172 @@
+var abi = [
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getMessage",
+      "outputs": [
+        {
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function",
+      "signature": "0xce6d41de"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "newMessage",
+          "type": "string"
+        }
+      ],
+      "name": "setMessage",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function",
+      "signature": "0x368b8772"
+    },
+    {
+          "constant": false,
+          "inputs": [
+            {
+              "name": "x",
+              "type": "string"
+            }
+          ],
+          "name": "setname",
+          "outputs": [],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [],
+          "name": "getname",
+          "outputs": [
+            {
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": false,
+          "inputs": [
+            {
+              "name": "x",
+              "type": "string"
+            }
+          ],
+          "name": "setemail",
+          "outputs": [],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [],
+          "name": "getemail",
+          "outputs": [
+            {
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": false,
+          "inputs": [
+            {
+              "name": "x",
+              "type": "uint256"
+            }
+          ],
+          "name": "setyear",
+          "outputs": [],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [],
+          "name": "getyear",
+          "outputs": [
+            {
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": false,
+          "inputs": [
+            {
+              "name": "x",
+              "type": "uint256"
+            }
+          ],
+          "name": "setsem",
+          "outputs": [],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [],
+          "name": "getsem",
+          "outputs": [
+            {
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": false,
+          "inputs": [
+            {
+              "name": "x",
+              "type": "uint256"
+            }
+          ],
+          "name": "setSPI",
+          "outputs": [],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [],
+          "name": "getSPI",
+          "outputs": [
+            {
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        }
+  ];
---
+++ main.js
@@ -0,0 +1,91 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCyRfKbGCVJ7aTKTrMH2O4uOiul8PxjkX4",
+    authDomain: "login-923e9.firebaseapp.com",
+    databaseURL: "https://login-923e9.firebaseio.com",
+    projectId: "login-923e9",
+    storageBucket: "login-923e9.appspot.com",
+    messagingSenderId: "775002041975",
+    appId: "1:775002041975:web:e99909857533fc2e316072",
+    measurementId: "G-DD9T0CNB61"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+const auth = firebase.auth();
+  // function signUp(){
+    
+  //   var email = document.getElementById("email");
+  //   var password = document.getElementById("password");
+    
+  //   const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+  //   promise.catch(e => alert(e.message));
+    
+  //   alert("Signed Up");
+  // }
+ function signIn(){
+    
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    
+    const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+    promise.catch(e => alert(e.message));
+
+    alert("Signed In");
+  }
+  function signOut(){
+    auth.signOut();
+    alert("Signed Out");
+    
+  }
+  // auth.onAuthStateChanged(function(user){
+    
+  //   if(user){
+      
+  //     var email = user.email;
+  //     alert("Active User " + email);
+
+  //     var email = user.email;
+  //     document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+      
+  //     //Take user to a different or home page
+
+  //     //is signed in
+      
+  //   }else{
+      
+  //     alert("No Active User");
+  //     //no user is signed in
+  //   }
+    
+    
+    
+  // });
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("formContainer").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("formContainer").style.display = "block";
+
+  }
+});
